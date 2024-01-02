e20b3ab059699edb0cff9a4d8f41ed01595d4aaa
Programming Wormhole <101206208+programmingwormhole@users.noreply.github.com>
2024-01-02 15:55:01 +0600

    Create example.dart

---
+++ example.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationServices {
+  final FlutterLocalNotificationsPlugin notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> initNotification() async {
+    AndroidInitializationSettings androidInitializationSettings =
+        const AndroidInitializationSettings('mipmap/ic_launcher');
+
+    DarwinInitializationSettings darwinInitializationSettings =
+        DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification: (id, title, body, payload) async {},
+    );
+
+    InitializationSettings initializationSettings = InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: darwinInitializationSettings,
+    );
+
+    await notificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse:
+          (NotificationResponse notificationResponse) async {},
+    );
+  }
+
+  notificationDetails() {
+    return const NotificationDetails(
+        android: AndroidNotificationDetails('channelId', 'channelName'),
+        iOS: DarwinNotificationDetails());
+  }
+
+  Future showNotification(
+      {int id = 0, String? title, String? body, String? payload}) async {
+    return notificationsPlugin.show(id, title, body, notificationDetails());
+  }
+}
